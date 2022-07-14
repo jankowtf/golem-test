442726c47806e48bc4bb0c24f6a0acadad9a8473
Janko Thyson <janko.thyson@rappster.io>
2022-07-14 12:11:48 +0200

    renv

--- dev/docker.sh
+++ dev/docker.sh
@@ -3,3 +3,6 @@ docker build -t golem-test .
 docker run -d --rm --name golem-test -e PORT=5000 -p 5001:5000 golem-test
 
 open http://localhost:5001
+
+# --- GCP ---
+docker tag rocker/shiny:4.0.0 eu.gcr.io/dbt-def/shiny:4.0.0
