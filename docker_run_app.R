c58653c595c1553b06d28bb85ad6b2137c929acb
Janko Thyson <janko.thyson@rappster.io>
2022-07-15 12:48:49 +0200

    Dockerfile v0.2

--- docker_run_app.R
+++ docker_run_app.R
@@ -1,2 +1,2 @@
-options(shiny.port = Sys.getenv("PORT"), shiny.host='0.0.0.0', shiny.fullstacktrace = TRUE)
+options(shiny.port = as.numeric(Sys.getenv("PORT")), shiny.host='0.0.0.0', shiny.fullstacktrace = TRUE)
 golem.test::run_app()
