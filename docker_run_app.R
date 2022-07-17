285fb768956829deceb1b4ff35b70a3cebb0fef2
Janko Thyson <janko.thyson@rappster.io>
2022-07-17 16:43:04 +0200

    Cleaned up YAMLs

--- docker_run_app.R
+++ docker_run_app.R
@@ -1,2 +1,4 @@
 options(shiny.port = as.numeric(Sys.getenv("PORT")), shiny.host='0.0.0.0', shiny.fullstacktrace = TRUE)
+print(getwd())
+print(as.data.frame(installed.packages()[,c(1,3)]))
 golem.test::run_app()
